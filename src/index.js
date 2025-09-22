f8238d9c035e8d3e2bd601f993f689baae500ca9
JASH MANDALIYA <167299436+Jashmandaliya18@users.noreply.github.com>
2025-09-22 19:17:14 +0530

    add user route and user controlller

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 // require("dotenv").config({ path: "./.env" });
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import express from 'express';
+// import {app} from "./app.js";
 
 dotenv.config({ path: "./.env" });
+
+const app = express();
+
 
 connectDB()
     .then(() => {
         app.listen(process.env.PORT || 8000, () => {
-            console.log(`Seerver is Running on port ${process.env.PORT || 8000}`);
+            console.log(`Server is Running on port ${process.env.PORT || 8000}`);
         })
     })
     .catch((err) => {
