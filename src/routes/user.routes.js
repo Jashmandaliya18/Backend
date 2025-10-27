1f302eeedeb84679d9466b26305901dac76704cb
JASH MANDALIYA <167299436+Jashmandaliya18@users.noreply.github.com>
2025-10-27 17:37:05 +0530

    Refactor user.routes.js: reorganize imports and add secured routes for account management

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,20 @@
 import { Router } from "express";
-import { loginUser, logOutUser, registerUser, refreshAccessToken } from "../controllers/user.controllers.js";
+import {
+    loginUser,
+    logOutUser,
+    registerUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    avatarUpdate,
+    coverImageUpdate,
+    getUserChannelProfile,
+    getWatchHistory
+} from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
+import { verify } from "jsonwebtoken";
 
 const router = Router();
 
@@ -21,8 +34,15 @@ router.route("/register").post(
 
 router.route("/login").post(loginUser)
 
+// Secured routes
 router.route("/logout").post(verifyJWT, logOutUser)
-
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/changePassword").post(verifyJWT, changeCurrentPassword)
+router.route("/currentUser").get(verifyJWT, getCurrentUser)
+router.route("/updateAccountDetails").patch(verifyJWT, updateAccountDetails)
+router.route("/Avatar").patch(verifyJWT, upload.single("avatar"), avatarUpdate)
+router.route("/coverImage").patch(verifyJWT, upload.single("coverImage"), coverImageUpdate)
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router; 
